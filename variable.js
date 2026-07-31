54a890184706acb2ea37c50d5cd978517374aa1a
amandangi202300@gmsil.com <amandangi202300@gmail.com>
2026-07-31 23:11:51 +0530

    some notes

--- variable.js
+++ variable.js
@@ -1,10 +1,23 @@
+// There are three types of variable let var const
 let a= 5;
 // let keyword are used in Javascript to store different type of value and 
 // the varible name of value is change with same name 
 a=6;
 console.log(a);
-document.write("Hello World");
+ //document.write("Hello World");
 // document.write() :-  Web page par output dikhata hai ,or ,User ko directly output dikhta hai, or , 
 // document object ka write() method hai
 // console.log() :- Browser ke Console mein output dikhata hai, or, Developer ko debugging ke liye output dikhta hai
 // console object ka log() method hai
+
+const b=45;
+// The Const Keyword is used for fixed vairibale i.e we cant used same variable name in same code
+// e.g b= 46; not run properly he provied error
+
+
+var c=78;
+// The var keyword is provied with same multiple variable name like this var a=25, var a=26 
+// this is demarit off var 
+// var is a old version keyword in Javascript 
+// This Keyword is not uderstan the any type of scope like global scope and functional Scope 
+
