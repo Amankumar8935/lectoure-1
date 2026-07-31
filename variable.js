f1119d24e10b1709e74ed7a65e113c2ea0262840
amandangi202300@gmsil.com <amandangi202300@gmail.com>
2026-07-31 08:37:36 +0530

    document.write add

--- variable.js
+++ variable.js
@@ -3,3 +3,8 @@ let a= 5;
 // the varible name of value is change with same name 
 a=6;
 console.log(a);
+document.write("Hello World");
+// document.write() :-  Web page par output dikhata hai ,or ,User ko directly output dikhta hai, or , 
+// document object ka write() method hai
+// console.log() :- Browser ke Console mein output dikhata hai, or, Developer ko debugging ke liye output dikhta hai
+// console object ka log() method hai
