5eec674a67faaaf5adf29d10e7d72bc30a2ae544
amandangi202300@gmsil.com <amandangi202300@gmail.com>
2026-08-02 00:20:29 +0530

    datatype

--- datatypes.js
+++ datatypes.js
@@ -1,26 +1,50 @@
 //there are two types of data types
-// 
+//
+// number 
 let a = 25;
 console.log(a);
-
+console.log(typeof a);
 let b=25.3;
 console.log(b);
+console.log(typeof b);
 
+// string
 let c= "this is javascript Code";
 console.log(c);
+console.log(typeof c);
 let d= 'Village';
 console.log(d);
+console.log(typeof d);
 
+//boolean
 let login= true;
 console.log(login);
+console.log(typeof login);
+
 let fa=false;
 console.log(fa);
+console.log(typeof fa);
+
+//undefined
 let user;
 console.log(user);
+console.log(typeof user);
+
+
+//bigint
 let num=235152632214541314n;
 console.log(num);
+console.log(typeof num);
+
+//symbole
 const id1=Symbol("id10");
 console.log(id1);
+console.log(typeof id1);
+
+//null
 let weather =null;
 console.log(weather);
+console.log(typeof weather);
+
+
 
