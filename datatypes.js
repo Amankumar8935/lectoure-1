c00dd4f7495f13191ffe24535842b58d112ce032
amandangi202300@gmsil.com <amandangi202300@gmail.com>
2026-08-02 00:01:50 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+//there are two types of data types
+// 
+let a = 25;
+console.log(a);
+
+let b=25.3;
+console.log(b);
+
+let c= "this is javascript Code";
+console.log(c);
+let d= 'Village';
+console.log(d);
+
+let login= true;
+console.log(login);
+let fa=false;
+console.log(fa);
+let user;
+console.log(user);
+let num=235152632214541314n;
+console.log(num);
+const id1=Symbol("id10");
+console.log(id1);
+let weather =null;
+console.log(weather);
+
