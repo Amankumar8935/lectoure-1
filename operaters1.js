38af5a88cf837537e67dd5dce6bb5751cbb94c21
amandangi202300@gmsil.com <amandangi202300@gmail.com>
2026-08-06 20:51:36 +0530

    inceremt decreemt

--- operaters1.js
+++ operaters1.js
@@ -8,4 +8,8 @@ console.log(2*5); // Multiplication Operator
 console.log(6/2); // Divided Operator
 console.log(5%2);  // Modulo Operator he store Reminder
 console.log(5**3); // ** squaring i.e 5 ka qube 
-
+let a=5;
+console.log(++a); // Increment Operator
+let b=6;
+console.log(--b); // Decrement Operator
+// 
