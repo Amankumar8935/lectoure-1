235c384dcffc5d198b58b99c348c82f07cef2dd2
amandangi202300@gmsil.com <amandangi202300@gmail.com>
2026-08-06 20:43:29 +0530

    Operaters files

---
+++ operaters1.js
@@ -0,0 +1,11 @@
+// operater 
+// Arithmetic Operator 
+
+
+console.log(2+5); // Addition Operator
+console.log(2-5); // Subtraticon Operator
+console.log(2*5); // Multiplication Operator 
+console.log(6/2); // Divided Operator
+console.log(5%2);  // Modulo Operator he store Reminder
+console.log(5**3); // ** squaring i.e 5 ka qube 
+
