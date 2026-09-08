efbf5164e7fac391236ec943e7cb93c3115efa02
Aman <amandangi202300@gmail.com>
2026-09-08 20:54:17 +0530

    prompt

--- first.js
+++ first.js
@@ -1,2 +1,7 @@
 console.log("Hello World");
 console.log("This is first java script code");
+
+let num = Number(prompt("Enter any number"));
+console.log(num); 
+ /*prompt is used for takeing input through the any browser */
+ 
